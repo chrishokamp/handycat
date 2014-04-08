cb99ad6ec540f5871214231310c6bd0eee5e8a89
Chris Hokamp <chris.hokamp@gmail.com>
2014-04-08 10:30:10 +0100

    prototype service for rule management

---
+++ app/scripts/services/ruleMap.js
@@ -0,0 +1,33 @@
+angular.module('services')
+.factory('ruleMap', ['$log', function($log) {
+
+// rules are objects with the form:
+//    { 'operationName': 'opName', 'context': '<context>, 'change': <change> }
+  return {
+    rules: [],
+    addRule: function(newRuleObj) {
+      // make sure we don't already have the rule
+      if (!this.contains(newRuleObj)) {
+        rules.push(newRuleObj);
+      }
+    },
+    contains: function(newRuleObj) {
+      return (_.find(this.rules, function(ruleObj) {
+        // check all rules for deep equality
+        return _.isEqual(newRuleObj, ruleObj);
+      }));
+    },
+    deleteRule: function(ruleObj) {
+      this.rules.splice(
+        _.indexOf(
+          this.rules,
+          _.find(this.rules, function(rule) { return _.isEqual(rule, ruleObj) })
+        ),
+        // splice it out
+        1
+      );
+    }
+  }
+
+
+}]);
