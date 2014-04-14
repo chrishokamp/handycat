d4284e42a3b206e66b19ea33647de18a4ca55267
Chris Hokamp <chris.hokamp@gmail.com>
2014-04-14 15:15:28 +0100

    added multilingual support to wiki concordancer

--- server/web.js
+++ server/web.js
@@ -1,5 +1,6 @@
 var gzippo = require('gzippo')
 	, express = require('express')
+	, params = require('express-params')
 	, http = require('http')
 	, getJSON = require('./getJSON')
 	, cors = require('cors')
@@ -9,31 +10,42 @@ var gzippo = require('gzippo')
 
 app.use(cors());
 app.use(app.router);
+params.extend(app);
 app.use(express.logger('dev'));
+// for hosting the app using express
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
 // add a route to query media wiki
-app.get('/wikipedia', function(req, res){
-	console.log('i just got a GET request to /wikipedia');
-	// the search parameter name is 'srsearch'
+app.param('lang', /^\w{2}$/);
+app.get('/wikipedia/:lang', function(req, res){
+  console.log('i just got a GET request to /wikipedia/:lang');
+  // the search parameter name is 'srsearch'
+  var lang = req.params.lang.toString().trim();
+  console.log('the req param is: ' + lang);
   console.log('the req query param is: ' + req.query.srsearch);
   // Question: put in quotes to search literally
   var searchQuery = encodeURIComponent(req.query.srsearch);
-	var query = '&srsearch=' + searchQuery;
+  var query = '&srsearch=' + searchQuery;
 
-
-	var options = {
-	  host: 'en.wikipedia.org',
-	  path: '/w/api.php?action=query&format=json&list=search&srprop=snippet' + query,
-	  method: 'GET'
-	};
-	getJSON.getJSON(options,
-		function(result) {
-			var searchResults = result.query.search;
-			//console.log(JSON.stringify(searchResults, null, 3));
-    		res.setHeader('Content-Type', 'application/json');
-    		res.send(searchResults);
-		});
+  var lang_host = '';
+  if(lang === 'en') {
+      console.log('lang is: en');
+      lang_host = 'en.wikipedia.org';
+  } else if (lang === 'de') {
+      lang_host = 'de.wikipedia.org';
+  }
+  var options = {
+    host: lang_host,
+    path: '/w/api.php?action=query&format=json&list=search&srprop=snippet' + query,
+    method: 'GET'
+  };
+  getJSON.getJSON(options,
+	function(result) {
+		var searchResults = result.query.search;
+		//console.log(JSON.stringify(searchResults, null, 3));
+   		res.setHeader('Content-Type', 'application/json');
+   		res.send(searchResults);
+	});
 
 });
 
