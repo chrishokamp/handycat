52d0acbbb13961bf8360f3d9b6603f7fc570d9f8
Ximo Planells <ximo.planells@gmail.com>
2014-06-17 11:43:49 +0100

    Random reordering deactivated

--- app/scripts/services/segmentOrder.js
+++ app/scripts/services/segmentOrder.js
@@ -15,7 +15,7 @@ angular.module('services').factory('SegmentOrder', ['$http', '$rootScope', 'base
         this.order.push(i);
 
       // testing only
-      this.order = this.shuffle(this.order);
+      //this.order = this.shuffle(this.order);
     },
     shuffle: function(o){
      for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
