a39587e9d2d93b4ba071f0b0d55496038d6352d9
Chris Hokamp <chris.hokamp@gmail.com>
2014-08-23 16:03:41 +0100

    cleanup

--- app/scripts/services/logger.js
+++ app/scripts/services/logger.js
@@ -1,9 +1,8 @@
-
 // the logger should log things that are NOT directly available in the XLIFF
 // the logger service also renders the json to the user
 // the rendered json is always in sync with the server
 
-// segments: [ {id: <id
+// segments: [ {id: <id>, source: <source>, hyp: <hypothesis>, final: <post-edited revision> } ]
 angular.module('services')
 .factory('Logger', ['Document', '$rootScope', '$log', function(Document, $rootScope, $log) {
 
@@ -11,11 +10,17 @@ angular.module('services')
     // segment completion is implicit in the data
 
     // Initialize with the source, hypothesis, and update when the user presses "complete"
-    // TODO: how are editing phases captured in the XLIFF standard?
+    // TODO: how are the different editing phases captured in the XLIFF standard?
+    // WORKING: how do we capture and save the state of segments?
+    // This is a big design decision which relies on degree to which we want to be dependent upon the XLIFF standard
+    // The design decision is whether to stick with XLIFF, or to design our own persistence model
+    // Question 1: does XLIFF even provide for the option to save both the original hypothesis, and the post-edited version?
+    // Question 2: Post-editing is different than translating from scratch -- if the user didn't post edit, the logs may not help nearly as much
+    // Sticking with XLIFF, and writing in- out- converters would be the industry and standard guys' preference
+    // How much more work will this be than creating an application-specific persistence format?
 
     // to initialize the logger when a new XLIFF document loads
     $rootScope.$on('document-loaded', function(e) {
-      $log.log('LOGGER INITIALIZED');
       angular.forEach(_.zip(Document.sourceSegments, Document.targetSegments, new Array(Document.sourceSegments.length)),
         function(tuple) {
           // the final field should be empty before the user starts editing
@@ -38,9 +43,5 @@ angular.module('services')
         var out = JSON.stringify(this.Log.segments, undefined, 2);
         return out;
       }
-
     }
-
-
-
 }]);
