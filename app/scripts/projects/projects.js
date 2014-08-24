21e397c9acb0dd212433ffc56001268b07f44592
Chris Hokamp <chris.hokamp@gmail.com>
2014-08-24 19:30:01 +0100

    project list from server

--- app/scripts/projects/projects.js
+++ app/scripts/projects/projects.js
@@ -1,9 +1,18 @@
 angular.module('controllers')
-.controller('ProjectCtrl', ['$scope', function($scope) {
+.controller('ProjectCtrl', ['$scope', 'Projects', '$log', function($scope, Projects, $log) {
     $scope.projects = [
       { "name": "test-project1"},
       { "name": "test-project2"},
       { "name": "test-project3"},
     ];
+
+         // Chris: this is called when the project-list template is initialized
+    $scope.find = function() {
+      Projects.query(function(projects) {
+        $log.log('PROJECTS');
+        $log.log(projects);
+        $scope.projects = projects;
+      });
+    };
 }]);
 
