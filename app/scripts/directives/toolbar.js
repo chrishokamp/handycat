ea2f97aa4b859e7c6cd4f3eb1dc226b1ea507b44
Chris Hokamp <chris.hokamp@gmail.com>
2014-10-07 08:28:09 +0100

    tm updates

--- app/scripts/directives/toolbar.js
+++ app/scripts/directives/toolbar.js
@@ -28,7 +28,7 @@ angular.module('directives').directive('toolbar', ['editSession', 'TranslationMe
 
         }
       )},0);
-
+//      {"provider":{"name":"Lingua Custodia","id":10635},"owner":{"name":"ECB","id":10975},"source":"Based on the reference amount of 4 million euro for the period 2002-2005 and 1 million euro for 2006, the annual appropriations authorised under the Pericles programme, were Euros 1.2 million for 2002; Euros 0.9 million for 2003;","industry":{"name":"Financials","id":12},"source_lang":{"name":"English (United States)","id":"en-us"},"target":"Sur la base du montant de référence de 4 millions d ' euros pour la période 2002-2005 et d ' un million d ' euros pour 2006, les crédits annuels autorisés dans le cadre du programme Pericles étaient de 1,2 millions d ' euros pour 2002, 0,9 million d ' euros pour 2003, 0,9 million d ' euros pour 2004, 1 million d ' euros pour 2005 et 1 million d ' euros pour 2006.","content_type":{"name":"Financial Documentation","id":10},"product":{"name":"Default","id":12512},"id":"en-us_fr-fr_11128729","target_lang":{"name":"French (France)","id":"fr-fr"}}
       var queryTM = function(query) {
         var queryObj = { userId: $scope.currentUser._id, sourceLang: 'en-US', targetLang: 'fr-FR', query: query};
         TranslationMemory.get(queryObj, function(tmResponse) {
