6fdcaaaebbf84d6af6ebd3bc2c37ff9cf2f66f9d
Chris Hokamp <chris.hokamp@gmail.com>
2015-01-28 16:36:11 +0000

    add divider between segments

---
+++ server/test/unit/memoryCache/memoryCache-spec.js
@@ -0,0 +1,20 @@
+// the cache synchronously loads a resource
+// using the express-redis-cache module
+
+
+// load and register the schemas first
+var fs = require('fs'),
+    path = require('path');
+
+var modelsPath = path.join(__dirname, '../../../models');
+fs.readdirSync(modelsPath).forEach(function (file) {
+    require(modelsPath + '/' + file);
+});
+var projects = require('../../../controllers/projects');
+
+describe("project controller functionality", function () {
+    it("can create a new project", function () {
+      console.log('loaded projects');
+          //expect(product).toBe(6);
+    });
+});
