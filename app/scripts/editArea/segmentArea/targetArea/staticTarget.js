4a2bdb73fc70020b164329ae4ec242d2fd787898
Chris Hokamp <chris.hokamp@gmail.com>
2015-03-10 22:11:40 +0000

    remove username from top toolbar

--- app/scripts/editArea/segmentArea/targetArea/staticTarget.js
+++ app/scripts/editArea/segmentArea/targetArea/staticTarget.js
@@ -4,27 +4,39 @@ angular.module('directives').directive('staticTarget', ['editSession', '$compile
     restrict: 'E',
     templateUrl: 'scripts/editArea/segmentArea/targetArea/static-target.html',
     link: function($scope,el){
+      // flag indicating whether components already exist on this element
+      $scope.subComponents = {
+        componentsExist: false
+      };
       // make the height the same as the source (max of source+target heights)
 
       // TODO: adding this component is slow
       // TODO: either we need to load it beforehand (i.e. one segment early), or we need to find the bottleneck (it's probably loading the ace editor)
+      // TODO: naively listening for click on this element causes it to be reloaded every time -- first check if the segment is active
       el.on('click', function() {
         $log.log('targetText was clicked...');
-        var newHtml = '<div ng-include="\'scripts/editArea/segmentArea/targetArea/target-area.html\'"></div>';
-        $log.log(newHtml);
-        var compiledHtml = $compile(newHtml)($scope);
-        // is $apply necessary because we're in jquery here?
-        $scope.$apply(function() {
-          el.replaceWith(compiledHtml);
-          session.setSegment($scope.id.index);
-        });
+        // init the components if they don't exist
+        if (!$scope.subComponents.componentsExist) {
+          var newHtml = '<div ng-include="\'scripts/editArea/segmentArea/targetArea/target-area.html\'"></div>';
+          $log.log(newHtml);
+          var compiledHtml = $compile(newHtml)($scope);
+          // is $apply necessary because we're in jquery here?
+          $scope.$apply(function() {
+            el.replaceWith(compiledHtml);
+            $scope.componentsExist = true;
+          });
+        }
+        session.setSegment($scope.id.index);
       });
 
       $scope.$on('activate', function() {
-        $log.log('the activate event fired');
-        var newHtml = '<div ng-include="\'scripts/editArea/segmentArea/targetArea/target-area.html\'"></div>';
-        var compiledHtml = $compile(newHtml)($scope);
-        el.replaceWith(compiledHtml);
+        if (!$scope.subComponents.componentsExist) {
+          $log.log('the activate event fired');
+          var newHtml = '<div ng-include="\'scripts/editArea/segmentArea/targetArea/target-area.html\'"></div>';
+          var compiledHtml = $compile(newHtml)($scope);
+          el.replaceWith(compiledHtml);
+          $scope.componentsExist = true;
+        }
       });
 
     }
