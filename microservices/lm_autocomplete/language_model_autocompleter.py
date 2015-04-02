3bb09a374dbcb1c831cf940290532706c22bf345
Chris Hokamp <chris.hokamp@gmail.com>
2015-04-02 20:16:50 +0100

    functional language model server

--- microservices/lm_autocomplete/language_model_autocompleter.py
+++ microservices/lm_autocomplete/language_model_autocompleter.py
@@ -17,20 +17,19 @@ class LanguageModelAutocompleter:
         # language_models are [{'lang_code': <lang_code>, 'srilm_lm_file': <srilm_lm_file>,
         # 'phrase_tables': {(source_lang, target_lang): phrase_table}]
         # phrase tables for lms must implement get_target_phrases
-        for k, v in language_models.items():
-            assert type(v) == str, 'language model file names must be strings'
 
         self.language_model_servers = {}
         self.start_server_template = "ngram -server-port {} -lm {}"
         self.call_server_template = "ngram -use-server {} -ppl {} -debug 2 -tolower"
 
         # start srilm servers for each of the language models
-        for language_model_data, port in zip(language_models, range(server_port_range_start, len(language_models))):
+        for language_model_data, port in zip(language_models, range(server_port_range_start, server_port_range_start + len(language_models))):
+            assert type(language_model_data['srilm_lm_file']) == str, 'language model file names must be strings'
             language_model_server = self._start_ngram_server(language_model_data['lang_code'],
                                                              language_model_data['srilm_lm_file'], port)
             # add the phrase table key to the server object
             language_model_server['phrase_tables'] = language_model_data['phrase_tables']
-            self.language_model_servers.update(language_model_server)
+            self.language_model_servers[language_model_data['lang_code']] = language_model_server
 
     # kill all of the server processes when this obj gets garbage collected
     def __del__(self):
@@ -40,7 +39,7 @@ class LanguageModelAutocompleter:
     def _start_ngram_server(self, lang_code, lm_file, port):
         start_server_command = self.start_server_template.format(port, lm_file)
         server_process = popen_obj = subprocess.Popen(start_server_command.split())
-        return {lang_code: {'process': server_process, 'port': port}}
+        return {'process': server_process, 'port': port}
 
     def _generate_completion_candidates(self, source_lang, target_lang, source_tokens=[]):
         """
@@ -48,16 +47,19 @@ class LanguageModelAutocompleter:
         :return: list
         """
         source_phrases = extract_phrases(source_tokens)
-        target_candidates = [self.language_models[target_lang]['phrase_tables'][(source_lang, target_lang)]
+        nested_target_candidates = [self.language_model_servers[target_lang]['phrase_tables'][(source_lang, target_lang)]
                                  .get_target_phrases(source_phrase)
                              for source_phrase in source_phrases]
+        # flatten and extract just the target phrase from the phrase table object
+        target_candidates = [c['target'] for l in nested_target_candidates for c in l]
         return target_candidates
 
     # generate ranked completions given the source segment and the current target prefix
     def get_ranked_completions(self, source_lang, target_lang, source_tokens=[], target_prefix=[]):
         cands = self._generate_completion_candidates(source_lang, target_lang, source_tokens)
         lm_server = self.language_model_servers[target_lang]
-        with tempfile.NamedTemporaryFile(mode='w', encoding='utf8') as options_file:
+        # with tempfile.NamedTemporaryFile(mode='w') as options_file:
+        with codecs.open('cands.tmp', 'w', encoding='utf8') as options_file:
             for candidate in cands:
                 completion = target_prefix + [candidate]
                 # remember that the lm is assumed to be _lowercase_
@@ -75,7 +77,7 @@ class LanguageModelAutocompleter:
         return sorted_completions
 
     @staticmethod
-    def _parse_srilm_output(self, srilm_output):
+    def _parse_srilm_output(srilm_output):
         # SRILM prints one blank line at the end of the file, ignore it
         output_lines = srilm_output.split('\n')[:-1]
 
