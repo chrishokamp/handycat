e744445d510563564801126dea9467c0cf68d7b7
Chris Hokamp <chris.hokamp@gmail.com>
2015-04-23 07:45:14 +0100

    updates before experiment 2

--- deployment/build_handycat.sh
+++ deployment/build_handycat.sh
@@ -3,7 +3,7 @@ cd ..
 grunt handycat_builds
 tar kczf handycat_test_build.20.4.15.tar.gz handycat_builds 
 
-scp -r -i /home/chris/amazon_aws/aws-handycat.pem handycat_test_build.20.4.15.tar.gz ubuntu@ec2-52-28-61-16.eu-central-1.compute.amazonaws.com:~
+scp -r -i /home/chris/amazon_aws/aws-handycat.pem handycat_test_build.20.4.15.tar.gz ubuntu@ec2-52-28-65-207.eu-central-1.compute.amazonaws.com:~
 
-ssh -i /home/chris/amazon_aws/aws-handycat.pem ubuntu@ec2-52-28-61-16.eu-central-1.compute.amazonaws.com  
+ssh -i /home/chris/amazon_aws/aws-handycat.pem ubuntu@ec2-52-28-65-207.eu-central-1.compute.amazonaws.com
 
