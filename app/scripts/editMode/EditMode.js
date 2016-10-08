4312ea5ac1c00b32326bd8fd3b4efce882a1d889
Chris Hokamp <chris.hokamp@gmail.com>
2016-10-08 16:32:29 +0100

    cleaning up a bit

--- app/scripts/editMode/EditMode.js
+++ app/scripts/editMode/EditMode.js
@@ -5,7 +5,6 @@ angular.module('controllers')
 
   // Working  - the EditModeCtrl provides the interface between the EditMode and the UI component
   // TODO: the code below was copied from AceCtrl
-  // the following functions expose control of the current mode on the $scope
   $scope.selectNextRange = function() {
     currentMode.selectNextTokenRange();
   };
