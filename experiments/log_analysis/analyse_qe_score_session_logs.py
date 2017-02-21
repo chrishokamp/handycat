91dbb30e5b77bc4818e54a722b6cf801607ac87b
Chris Hokamp <chris.hokamp@gmail.com>
2017-02-21 14:29:53 +0000

    update log analysis script

--- experiments/log_analysis/analyse_qe_score_session_logs.py
+++ experiments/log_analysis/analyse_qe_score_session_logs.py
@@ -14,6 +14,7 @@ import difflib
 
 __author__ = "Chris Hokamp"
 
+
 def tasks_from_session_log(log):
     return [(name, log['document'][name]['segments']) for name in log['document'].keys()]
 
@@ -123,6 +124,17 @@ def get_before_after_from_segments(segments):
     return segment_before_after
 
 
+def get_username_from_segments(segments):
+    """just a utility to write the username for each segment to make analysis easier"""
+    ordered_segments = sorted([(int(k), v) for k,v in segments.items()], key=lambda x: x[0])
+    segment_usernames = []
+    for seg_id, events in ordered_segments:
+      seg_username = events[0]['user']['name']
+      segment_usernames.append((seg_id, seg_username))
+
+    return segment_usernames
+
+
 def get_edit_distance(before, after):
     matcher = difflib.SequenceMatcher(isjunk=None, a=before, b=after)
     return 1. - matcher.ratio()
@@ -130,9 +142,12 @@ def get_edit_distance(before, after):
 
 def main(logdir):
     logfiles = [os.path.join(logdir, f) for f in os.listdir(logdir)
-                if os.path.isfile(os.path.join(logdir, f))]
+                if os.path.isfile(os.path.join(logdir, f)) and '.json' in f]
     json_logs = [json.loads(codecs.open(f, encoding='utf8').read()) for f in logfiles]
     tasks_by_log = [tasks_from_session_log(log) for log in json_logs]
+
+    username_by_task_by_log = [[(name, get_username_from_segments(segments)) for name, segments in log]
+                                for log in tasks_by_log]
     editing_times_by_task_by_log = [[(name, get_time_spent_editing(segments)) for name, segments in log]
                                       for log in tasks_by_log]
     time_until_score_clicked_by_task_by_log = [[(name, get_time_until_score_clicked(segments)) for name, segments in log]
@@ -148,15 +163,16 @@ def main(logdir):
             task_name = re.sub(u' \| ', u'_', task_name)
             task_name = re.sub(u' ', u'_', task_name)
             rows = []
-            rows.append(['segment', 'time_until_score_accept', 'editing_time',
-                         'num_keystrokes', 'original', 'post_edited', 'edit_distance'])
+            rows.append(['segment', 'username', 'time_until_score_accept', 'editing_time',
+                         'num_keystrokes', 'edit_distance', 'original_mt', 'post_edited'])
             for seg_id in sorted([int(k) for k in segments.keys()]):
+                _, username = username_by_task_by_log[i][j][1][seg_id]
                 _, time_until_score_accept = time_until_score_clicked_by_task_by_log[i][j][1][seg_id]
                 _, editing_time = editing_times_by_task_by_log[i][j][1][seg_id]
                 _, num_keystrokes = num_keystrokes_by_task_by_log[i][j][1][seg_id]
                 _, before_, after_ = before_after_by_task_by_log[i][j][1][seg_id]
                 edit_distance = get_edit_distance(before_, after_)
-                new_row = [seg_id, time_until_score_accept, editing_time, num_keystrokes, before_, after_, edit_distance]
+                new_row = [seg_id+1, username, time_until_score_accept, editing_time, num_keystrokes, edit_distance, before_, after_]
                 rows.append(new_row)
 
             output_file = task_name
@@ -168,7 +184,7 @@ def main(logdir):
             output_file = output_file
             with codecs.open(output_file + '.tsv', 'w', encoding='utf8') as out:
                 for row in rows:
-                    out.write(u'{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(*row))
+                    out.write(u'{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(*row))
             already_used_filenames.update([output_file])
             print('Wrote {}'.format(output_file))
 
